9162fb0014681b420fa9faa2edb8bdc7addae888
TonyDecvA180XN <tony.dospiekhov@gmail.com>
2021-09-21 15:53:33 +0300

    Add glfw library

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,10 +1,38 @@
 #include "main.h"
-#include <iostream>
+#include <GLFW/glfw3.h>
 
-int main()
+int main(void)
 {
-	std::cout << "Hello world." << std::endl;
-	int a = 5;
-	std::cout << "The factorial of " << a << " is " << factorial(a) << "." << std::endl;
-	return 0;
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
