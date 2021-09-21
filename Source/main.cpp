feca3df9821ea1cb5e1718a9548ebb6be481d07b
TonyDecvA180XN <tony.dospiekhov@gmail.com>
2021-09-21 12:13:40 +0300

    Fix code dependencies

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,11 +1,10 @@
 #include "main.h"
 #include <iostream>
-#include "fmt/format.h"
 
 int main()
 {
 	std::cout << "Hello world." << std::endl;
 	int a = 5;
-	fmt::print("The factorial of {} is {}.", a, factorial(a));
+	std::cout << "The factorial of " << a << " is " << factorial(a) << "." << std::endl;
 	return 0;
 }
